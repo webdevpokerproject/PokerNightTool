b1e1742f47207b171c78e690ed34c03060d4b850
Maurice Ponte <maurice.leon.ponte@gmail.com>
2019-03-15 13:31:00 +0100

    Add prokerproject to repo

---
+++ PokerProjectWebsite/PokerProjectWebsite/App_Code/Functie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using WebMatrix.Data;
+
+/// <summary>
+/// Binnen deze class alle functionaliteit wegschrijven zodat de uiteindelijke pages overzichtelijk blijven.
+/// Gelieve alle methodes voorzien van XML-comments en eventueel aanvullen in de methode als deze complex is. 
+/// 
+/// Om een methode aan te roepen (binnen of buiten deze class) werkt als volgt: 
+/// ** Functie.AangemaakteMethode(* eventuele parameters*) ** 
+/// 
+/// Aangemaakte methodes dienen met een Hoofdletter te beginnen en in CamelCase stijl gemaakt worden.
+/// Parameters dienen een logische naam te hebben. Zie Login methode. 
+/// Voorbeeld: 
+/// ** AangemaakteMethode() ** 
+/// 
+/// Methodes die enkel queries uitvoeren dienen met QR_ te beginnen.
+/// Voorbeeld: 
+/// ** QR_HaalUniekeCodeOp() ** 
+/// 
+/// </summary>
+public class Functie
+{
+    /// <summary>
+    /// Deze 2 strings vertegenwoordigen de connectie string en database provider. 
+    /// Deze hiermee kan de database connectie op onderstaande manier worden aangeroepen:
+    /// ** Database db = Database.OpenConnectionString(Functions.connectionString, Functions.provider); ** 
+    /// </summary>
+    public const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\data.mdf;Integrated Security=True";
+    public const string provider = "System.Data.SqlClient";
+
+    /// <summary>
+    /// Zorgt voor inlogfunctionaliteit voor de beheerder. 
+    /// </summary>
+    /// <param name="naam"></param>
+    /// <param name="wachtwoord"></param>
+    /// <returns> true als output niet null is (en dus de record bestaat), false als dit niet het geval is </returns>
+    public static bool Login(string naam, string wachtwoord)
+    {
+        Database db = Database.OpenConnectionString(Functie.connectionString, Functie.provider);
+        string QR_GetLogin = "SELECT Gebruikersnaam, Wachtwoord FROM Beheerder WHERE Gebruikersnaam = @0 AND Wachtwoord = @1";
+        dynamic Output = db.QuerySingle(QR_GetLogin, naam, wachtwoord); 
+        if(Output != null)
+        {
+            return true;
+        }
+        return false; 
+    }
+}
